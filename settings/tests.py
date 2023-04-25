f4d67dcf816c1ef1c3d059964bd75d70f091ff3a
Fares Emad <faresemadx@gmail.com>
2023-04-25 11:35:23 +0200

    write unit test for settings app

--- settings/tests.py
+++ settings/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import About
 
-# Create your tests here.
+class AboutModelTest(TestCase):
+
+    def setUp(self):
+        self.about = About.objects.create(
+            title='Test Title',
+            description='Test Description',
+            email='test@test.com'
+        )
+
+    def test_about_model(self):
+        self.assertEqual(str(self.about), self.about.title)
+        self.assertEqual(self.about.description, 'Test Description')
+        self.assertEqual(self.about.email, 'test@test.com')
