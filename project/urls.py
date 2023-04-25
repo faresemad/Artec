23a0e348d345d4862e6b1e45ca3afce2677c0f52
Fares Emad <faresemadx@gmail.com>
2023-04-25 11:36:04 +0200

    add doesn't exist page&url

--- project/urls.py
+++ project/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path, include
+from django.urls import path, include, re_path
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -17,6 +18,8 @@ urlpatterns = [
     # API schema
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    # Invalid path
+    re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
