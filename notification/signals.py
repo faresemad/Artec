b6be77a23db63df9afad2f5cb6b903438263f0df
Fares Emad <faresemadx@gmail.com>
2023-06-17 22:19:36 +0200

    feat: add notification for new student and new user

--- notification/signals.py
+++ notification/signals.py
@@ -1,41 +1,47 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib.auth import get_user_model
 from notifications.signals import notify
 from students.models import Student
-from django.contrib.auth import get_user_model
 
 User = get_user_model()
+
+
+@receiver(post_save, sender=User)
+def send_welcome_notification(sender, instance, created, **kwargs):
+    if created:
+        notify.send(
+            sender=instance,
+            recipient=instance,
+            verb=f"مرحبا {instance.name}",
+            description="مرحبا بك في منصنتنا الجديدة",
+            level="success",
+        )
 
 
 @receiver(post_save, sender=Student)
 def send_notification(sender, instance, created, **kwargs):
     if created:
-        # Send a notification using the notify function
-        notify(
-            actor=instance.user,
+        notify.send(
+            sender=instance.user,
             recipient=instance.user,
-            verb="created",
-            target=instance,
-            description="A new student has been created",
+            verb=f"تهانينا علي انضمامك الي كلية '{instance.college}'",
+            description=f"تم انشاء طالب جديد بإسم '{instance.full_name}'",
+            level="success",
         )
-
-
-@receiver(post_save, sender=Student)
-def send_student_notification(sender, instance, created, **kwargs):
-    if created:
-        if instance.up_to_level:
-            notify(
-                actor=instance.user,
-                recipient=instance.user,
-                verb="became fit",
-                target=instance,
-                description="You have become fit to move up to the next level",
-            )
-        else:
-            notify(
-                actor=instance.user,
-                recipient=instance.user,
-                verb="became unfit",
-                target=instance,
-                description="You are still unfit to move up to the next level",
-            )
+    elif instance.up_to_level:
+        notify.send(
+            sender=instance.user,
+            recipient=instance.user,
+            verb=f"تهانينا{instance.full_name}",
+            description="انت الآن لائق للترقي لهذه الكلية",
+            level="success",
+        )
+    elif not instance.up_to_level:
+        notify.send(
+            sender=instance.user,
+            recipient=instance.user,
+            verb="مازلت غير لائق",
+            description="انت غير لائق للترقي لهذه الكلية",
+            level="warning",
+        )
