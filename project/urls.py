ed700e7712bf9a72779bd6421ddf9092e1510461
Fares Emad <faresemadx@gmail.com>
2023-06-17 14:39:42 +0200

    fix: resolve registeration and delete urls from settings

--- project/urls.py
+++ project/urls.py
@@ -11,14 +11,9 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api-auth/", include("rest_framework.urls")),
-    path("auth/", include("djoser.urls")),
-    path("auth/", include("djoser.urls.jwt")),
-    path("auth/", include("djoser.urls.authtoken")),
     path("user/", include("user.urls")),
     path("api/v1/notifications/", include("notification.urls")),
     path("api/v1/colleges/", include("colleges.urls")),
-    # path('api/v1/exams/', include("exams.urls")),
     path("api/v1/settings/", include("settings.urls")),
     path("api/v1/students/", include("students.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
